14c0044e47a1504caabe133e09f8bfcece924411
Washigton Gonçalves <washigtongoncalves@gmail.com>
2019-01-17 21:30:36 -0200

    Criando categorias para teste

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(CodeShopping\Models\Category::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->colorName,
+        'slug' => 'slug'
     ];
 });
