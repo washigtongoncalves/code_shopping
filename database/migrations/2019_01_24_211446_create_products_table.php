0667d95c3d22413c9b79e51f1cf4b0aa4ecc61bb
Washigton Gonçalves <washigtongoncalves@gmail.com>
2019-01-24 22:12:56 -0200

    Product migration

---
+++ database/migrations/2019_01_24_211446_create_products_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description');
+            $table->decimal('price', 16, 2)->default(0.00);
+            $table->integer('stock')->default(0);
+            $table->boolean('active')->default(1);
+            // $table->unique('name', 'uk_products_name');
+            // $table->unique('slug', 'uk_products_slug');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
