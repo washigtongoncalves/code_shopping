ecc3ddc3a9cc0e0fa49f9ce6b21d5bc7b06c5d45
Washigton Gonçalves <washigtongoncalves@gmail.com>
2019-02-05 22:21:59 -0200

    Endpoint para inserir categorias em produtos

--- app/Http/Controllers/Api/ProductCategoryController.php
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -4,6 +4,7 @@ namespace CodeShopping\Http\Controllers\Api;
 
 use CodeShopping\Http\Controllers\Controller;
 use CodeShopping\Models\Product;
+use CodeShopping\Models\Category;
 use Illuminate\Http\Request;
 
 class ProductCategoryController extends Controller
@@ -13,9 +14,12 @@ class ProductCategoryController extends Controller
         return $product->categories;
     }
 
-    public function store(Request $request)
+    public function store(Request $request, Product $product)
     {
-        //
+        $changed = $product->categories()->sync($request->get('categories'));
+        $categoriesAttachedId = $changed['attached'];
+        $categories = Category::whereIn('id', $categoriesAttachedId)->get();
+        return $categories->count() ? response()->json($categories, 201) : $categories;
     }
 
     public function destroy($id)
