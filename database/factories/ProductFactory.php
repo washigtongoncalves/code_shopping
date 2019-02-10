2d413d569f05633b98debf6abaad51e26e44a15d
Washigton Gonçalves <washigtongoncalves@gmail.com>
2019-02-10 21:30:29 -0200

    Stock de produtos com valores entre 100 e 1000

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,8 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(CodeShopping\Models\Product::class, function (Faker $faker) {
     return [
-        'name' => $faker->company,
-        'slug' => $faker->slug,
+        'name'  => $faker->company,
+        'slug'  => $faker->slug,
+        'stock' => $faker->numberBetween(100, 1000),
         'description' => $faker->paragraph(2),
         'price' => $faker->randomFloat(2, 100, 1000)
     ];
