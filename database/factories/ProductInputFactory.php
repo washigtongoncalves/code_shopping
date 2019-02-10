c48bc5de6536561de2e338bb5a57591828e7d191
Washigton Gonçalves <washigtongoncalves@gmail.com>
2019-02-10 19:25:00 -0200

    Melhorias na seeder de ProductInputs

--- database/factories/ProductInputFactory.php
+++ database/factories/ProductInputFactory.php
@@ -2,12 +2,9 @@
 
 use Faker\Generator as Faker;
 use CodeShopping\Models\ProductInput;
-use CodeShopping\Models\Product;
 
 $factory->define(ProductInput::class, function (Faker $faker) {
-    $products = Product::all();
     return [
-        'amount'     => $faker->numberBetween(1, 10000),
-        'product_id' => $products->random()->id
+        'amount' => $faker->numberBetween(1, 100)
     ];
 });
--- database/seeds/ProductInputsTableSeeder.php
+++ database/seeds/ProductInputsTableSeeder.php
@@ -2,11 +2,16 @@
 
 use Illuminate\Database\Seeder;
 use CodeShopping\Models\ProductInput;
+use CodeShopping\Models\Product;
 
 class ProductInputsTableSeeder extends Seeder
 {
     public function run()
     {
-        factory(ProductInput::class, 100)->create();
+        $products = Product::all();
+        factory(ProductInput::class, 100)->make()->each(function(ProductInput $productInput) use($products) {
+            $productInput->product_id = $products->random()->id;
+            $productInput->save();
+        });
     }
 }
