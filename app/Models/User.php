0c211b62e97eb154aee470ef6695febdd06a3e26
Washigton Gonçalves <washigtongoncalves@gmail.com>
2019-03-02 22:12:52 -0300

    Sujeito do token

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,9 @@ namespace CodeShopping\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable, SoftDeletes;
 
@@ -25,5 +26,18 @@ class User extends Authenticatable
     public function setPasswordAttribute($value)
     {
         $this->attributes['password'] = bcrypt($value);
+    }
+
+    public function getJWTCustomClaims() : array 
+    {
+        return [
+            'name'  => $this->name,
+            'email' => $this->email,
+        ];
+    }
+
+    public function getJWTIdentifier() 
+    {
+        return $this->id;
     }
 }
