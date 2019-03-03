c669f093bf6efdede96771f6ee6b997aef056504
Washigton Gonçalves <washigtongoncalves@gmail.com>
2019-03-02 23:11:29 -0300

    Protegendo API

--- routes/api.php
+++ routes/api.php
@@ -18,16 +18,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['namespace' => 'Api', 'as' => 'api.'], function() {
-    Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
-    // O método apiResouce já exclui as action create e edit
-    Route::patch('products/{product}/restore', 'ProductController@restore')->name('products.restore');
-    Route::apiResource('products', 'ProductController');
-    Route::resource('products.categories', 'ProductCategoryController', ['only' => ['index', 'store', 'destroy']]);
-    Route::resource('products.photos', 'ProductPhotoController', ['except' => ['create', 'edit']]);
-    Route::resource('categories.products', 'CategoryProductController', ['only' => ['index', 'store', 'destroy']]);
-    Route::resource('inputs' , 'ProductInputController' , ['only' => ['index', 'store', 'show']]);
-    Route::resource('outputs', 'ProductOutputController', ['only' => ['index', 'store', 'show']]);
     Route::post('login', 'AuthController@login')->name('login');
-    Route::patch('users/{user}/restore', 'UserController@restore')->name('users.restore');
-    Route::apiResource('users', 'UserController');
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
+        Route::patch('products/{product}/restore', 'ProductController@restore')->name('products.restore');
+        Route::apiResource('products', 'ProductController');
+        Route::resource('products.categories', 'ProductCategoryController', ['only' => ['index', 'store', 'destroy']]);
+        Route::resource('products.photos', 'ProductPhotoController', ['except' => ['create', 'edit']]);
+        Route::resource('categories.products', 'CategoryProductController', ['only' => ['index', 'store', 'destroy']]);
+        Route::resource('inputs' , 'ProductInputController' , ['only' => ['index', 'store', 'show']]);
+        Route::resource('outputs', 'ProductOutputController', ['only' => ['index', 'store', 'show']]);
+        Route::patch('users/{user}/restore', 'UserController@restore')->name('users.restore');
+        Route::apiResource('users', 'UserController');
+    });
 });
