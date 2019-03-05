29b5b49b400f55410696408d15be8f562627c1a6
Washigton Gonçalves <washigtongoncalves@gmail.com>
2019-03-04 22:51:02 -0300

    Configuração do CORS

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CodeShopping\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Closure;
+
+class CorsMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->is('api/*')) {
+            // Libera o request para qualquer origem
+            header('Access-Control-Allow-Origin: *');
+            // Libera o uso do header Content-Type
+            header('Access-Control-Allow-Headers: Content-Type');
+        }
+        return $next($request);
+    }
+}
