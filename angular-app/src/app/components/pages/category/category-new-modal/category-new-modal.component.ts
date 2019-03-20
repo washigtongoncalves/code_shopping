95ff1cf9ed7896b33a0031bdf0fb595215627d00
Washigton <washigton@setrasolucoes.com.br>
2019-03-20 11:43:08 -0300

    Resetar o campo após o cadastro de categoria

--- angular-app/src/app/components/pages/category/category-new-modal/category-new-modal.component.ts
+++ angular-app/src/app/components/pages/category/category-new-modal/category-new-modal.component.ts
@@ -5,72 +5,76 @@ import { CategoryHttpService } from 'src/app/services/http/category-http.service
 import { CategoryInterface } from 'src/app/interfaces/category.interface';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'category-new-modal',
   templateUrl: './category-new-modal.component.html',
   styleUrls: ['./category-new-modal.component.css']
 })
-export class CategoryNewModalComponent 
-{
+export class CategoryNewModalComponent {
+
   public category: CategoryInterface = {
-      'name': '',
-      'active' : true
+    name: '',
+    active : true
   };
-  
+
   @ViewChild(ModalComponent)
   private modal: ModalComponent;
-  
-  @Output() 
+
+  // tslint:disable-next-line:no-output-on-prefix
+  @Output()
   onSuccess: EventEmitter<any> = new EventEmitter<any>();
-  
-  @Output() 
+
+  // tslint:disable-next-line:no-output-on-prefix
+  @Output()
   onError: EventEmitter<HttpErrorResponse> = new EventEmitter<HttpErrorResponse>();
-  
+
   constructor(private categoryHttp: CategoryHttpService) { }
-  
-  submit()
-  {
-      let success = (category) => {
-          this.onSuccess.emit(category);
-          this.modal.hide();
-      };
-      let error = (err) => this.onError.emit(err);
-      this.categoryHttp
-          .create(this.category)
-          .subscribe(success, error);
+
+  submit() {
+    const success = (category) => {
+        this.onSuccess.emit(category);
+        this.modal.hide();
+        this.reset();
+    };
+    const error = (err) => this.onError.emit(err);
+    this.categoryHttp
+        .create(this.category)
+        .subscribe(success, error);
   }
-  
-  showModal()
-  {
-      this.modal.show();
+
+  reset() {
+    this.category = {
+        name: '',
+        active: true
+    };
   }
-  
-  hideModal()
-  {
-      this.modal.hide();
+
+  showModal() {
+    this.modal.show();
   }
-  
+
+  hideModal() {
+    this.modal.hide();
+  }
+
   // Eventos do componente de Modal
-  onShowModal($event: Event)
-  {
-      console.log('Show New Modal Event');
-      console.log($event);
+  onShowModal($event: Event) {
+    console.log('Show New Modal Event');
+    console.log($event);
   }
-  
-  onShownModal($event: Event)
-  {
-      console.log('Shown New Modal Event');
-      console.log($event);
+
+  onShownModal($event: Event) {
+    console.log('Shown New Modal Event');
+    console.log($event);
   }
-  
-  onHiddenModal($event: Event)
-  {
-      console.log('Hidden New Modal Event');
-      console.log($event);
+
+  onHiddenModal($event: Event) {
+    console.log('Hidden New Modal Event');
+    console.log($event);
   }
-  
-  onHideModal($event: Event)
-  {
-      console.log('Hide New Modal Event');
-      console.log($event);
+
+  onHideModal($event: Event) {
+    console.log('Hide New Modal Event');
+    console.log($event);
   }
 }
