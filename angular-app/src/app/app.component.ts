6b46338dc9aeb68687e197066a9423874c9fd2f6
Washigton Gonçalves <washigtongoncalves@gmail.com>
2019-03-19 22:36:30 -0300

    Criando barra de progress ao iniciar Ajax

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import pace from 'pace';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-app';
+export class AppComponent implements OnInit
+{
+    title = 'angular-app';
+
+    ngOnInit()
+    {
+        pace.start({
+            document: false
+        });
+    }
 }
