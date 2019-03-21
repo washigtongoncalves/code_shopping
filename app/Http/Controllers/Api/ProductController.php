0e0b9f99b7f9dcb452ca4d8694a2119aa896b4f3
Washigton Gonçalves <washigtongoncalves@gmail.com>
2019-03-20 23:09:02 -0300

    Exibir 5 produtos a cada request paginado

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
     {
         $query = Product::query();
         $query = $this->onlyTrashedIfRequested($query);
-        $products = $query->paginate(10);
+        $products = $query->paginate(5);
         return ProductResource::collection($products);
     }
 
