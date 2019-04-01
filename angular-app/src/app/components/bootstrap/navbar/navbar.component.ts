728470236307fcac889a55606d89cc3ff0b4ec8d
Washigton <washigton@setrasolucoes.com.br>
2019-04-01 16:59:19 -0300

    Idem ao commit anterior

--- angular-app/src/app/components/bootstrap/navbar/navbar.component.ts
+++ angular-app/src/app/components/bootstrap/navbar/navbar.component.ts
@@ -4,12 +4,14 @@ import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
+  // tslint:disable-next-line:no-input-rename
   @Input('app-title')
   appTitle: string;
 
