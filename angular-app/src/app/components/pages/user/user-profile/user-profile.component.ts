b17f90f4b56819cfe439c90adab8a6b264f7582d
Washigton <washigton@setrasolucoes.com.br>
2019-05-22 16:45:08 -0300

    .

--- angular-app/src/app/components/pages/user/user-profile/user-profile.component.ts
+++ angular-app/src/app/components/pages/user/user-profile/user-profile.component.ts
@@ -1,8 +1,8 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpErrorResponse } from '@angular/common/http';
 import { UserHttpService } from 'src/app/services/http/user-http.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { NotifyMessageService } from '../../../../services/notify-message.service';
 import fieldsOptions from '../user-form/user-fields-options';
 
 @Component({
@@ -15,43 +15,39 @@ export class UserProfileComponent {
 
   public form: FormGroup;
   private userId: number;
-
-  // tslint:disable-next-line:no-output-on-prefix
-  @Output()
-  onSuccess: EventEmitter<any> = new EventEmitter<any>();
-
-  // tslint:disable-next-line:no-output-on-prefix
-  @Output()
-  onError: EventEmitter<HttpErrorResponse> = new EventEmitter<HttpErrorResponse>();
   errors = {};
 
   constructor(
     private authService: AuthService,
-    private userHttp: UserHttpService
+    private userHttp: UserHttpService,
+    private notify: NotifyMessageService
   ) {
     this.userId = this.authService.me.id;
     const maxlength: number = fieldsOptions.name.validationMessage.maxlength;
     this.form = new FormBuilder().group({
       name:  ['', [Validators.required, Validators.maxLength(maxlength)]],
-      email: ['', [Validators.required, Validators.email]],
+      email: '',
       password: ''
     });
     this.form.patchValue(this.authService.me);
   }
 
   submit() {
-    const success = (user) => {
-        this.onSuccess.emit(user);
-        this.authService.me = user;
+    const success = (u) => {
+        this.notify.success('Perfil atualizado com sucesso!');
     };
     const error = (responseError) => {
       if (responseError.status === 422) {
         this.errors = responseError.error.errors;
       }
-      this.onError.emit(responseError);
+      this.notify.error('Ocorreu um erro ao atualizar o seu perfil!');
     };
+    const user = this.form.value;
+    if (user.password === '') {
+      delete user.password;
+    }
     this.userHttp
-        .update(this.userId, this.form.value)
+        .update(this.userId, user)
         .subscribe(success, error);
   }
 
