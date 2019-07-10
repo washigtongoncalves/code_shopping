acae312c0519398d6b429ac078f9617d5ba5e7ec
Washigton <washigton@setrasolucoes.com.br>
2019-07-10 17:48:22 -0300

    Mostrando lista de opções de login

--- whatsapp/src/pages/login-options/login-options.ts
+++ whatsapp/src/pages/login-options/login-options.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
 
 /**
  * Generated class for the LoginOptionsPage page.
@@ -15,11 +15,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginOptionsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private actionSheetCtrl: ActionSheetController
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginOptionsPage');
   }
 
+  openLoginOptions() {
+    const actionSheet = this.actionSheetCtrl.create({
+      title: 'Já possui telefone cadastrado?',
+      buttons: [
+        { text: 'Já tenho, quero logar' },
+        { text: 'Já tenho, quero trocar o telefone' },
+        { text: 'Não tenho, quero criar uma conta' },
+        { text: 'Cancelar', role: 'cancel' }
+      ]
+    });
+    actionSheet.present();
+  }
 }
