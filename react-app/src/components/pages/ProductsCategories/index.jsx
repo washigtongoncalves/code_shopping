ec3101f16e03eb45848dba000847da894b802f39
Washigton Gonçalves <washigtongoncalves@gmail.com>
2019-12-12 00:12:31 -0300

    Listar as categorias vinculadas ao produto

--- react-app/src/components/pages/ProductsCategories/index.jsx
+++ react-app/src/components/pages/ProductsCategories/index.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import NotifyMessageService from '../../../services/NotifyMessageService';
 import ProductsCategoriesServices from '../../../services/ProductsCategoriesServices';
+import { dateFormatBr } from '../../../functions/formater';
 
 const INITIAL_STATE = {
     product: [],
@@ -31,11 +33,76 @@ class ProductsCategories extends Component {
         });
     }
 
+    renderRows() {
+
+        const { product, categories } = this.state;
+        
+        if (!categories.length) {
+            return (
+                <tr key={0}>
+                    <td colSpan="5">
+                        Nenhuma categoria vinculada ainda :(
+                    </td>
+                </tr>
+            );
+        }
+
+        return categories.map(category => (
+            <tr key={category.id}>
+                <td data-title="ID:">
+                  {category.id}
+                </td>
+                <td data-title="Nome:">
+                    {category.name}
+                </td>
+                <td data-title="Ativa:">
+                    <i className={'fa ' + (category.active ? 'fa-check' : 'fa-times')}></i>
+                </td>
+                <td data-title="Criada em:">
+                    {dateFormatBr(category.created_at.date)}
+                </td>
+                <td data-title="Ações: ">
+                    <button type="button" className="btn btn-sm btn-danger btn-actions" 
+                        title={`Desvincular do produto ${product.name}`}
+                        onClick={() => this.showModalDelete(product)}>
+                        <i className="fa fa-trash-o"></i>
+                    </button>
+                </td>
+            </tr>
+        ));
+    }
+
     render() {
-        const product = this.state.product;
+        const { product } = this.state;
         return (
             <div className="row" id="no-more-tables">
                 <h3>Categorias do produto {product.name}</h3>
+                <table className="table table-striped table-hover table-sm">
+                    <thead>
+                        <tr>
+                            <td colSpan="5">
+                                <button className="btn btn-primary">
+                                    Vincular
+                                </button>
+                                <Link to="/products/list">
+                                    <button className="btn">
+                                        Voltar
+                                    </button>
+                                </Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th style={{ width: "5%"  }}>ID</th>
+                            <th style={{ width: "30%" }}>Nome</th>
+                            <th style={{ width: "15%" }}>Ativa?</th>
+                            <th style={{ width: "25%" }}>Criada em</th>
+                            <th style={{ width: "25%" }}>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       {this.renderRows()} 
+                    </tbody>
+                </table>
             </div>
         );
     }
