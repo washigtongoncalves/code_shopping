111cd5484bf90af8f747c81c760765d49d51aa67
Washigton Gonçalves <washigtongoncalves@gmail.com>
2020-01-06 22:11:37 -0300

    Layout da tela de login

--- react-app/src/components/pages/Users/Login/index.jsx
+++ react-app/src/components/pages/Users/Login/index.jsx
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
 
+import './floating-label.css';
+
 class Login extends Component {
     render() {
-        return (<div>Login</div>);
+        return (
+            <form className="form-signin">
+                <div className="text-center mb-4">
+                    <h1 className="h3 mb-3 font-weight-normal">
+                        <i className="fa fa-code"></i> Code Shopping
+                    </h1>
+                </div>
+                <div className="form-label-group">
+                    <input type="email" 
+                           id="inputEmail" 
+                           className="form-control" 
+                           placeholder="Informe seu e-mail" 
+                           required 
+                           autofocus />
+                    <label for="inputEmail">
+                        Seu e-mail
+                    </label>
+                </div>
+                <div className="form-label-group">
+                    <input type="password" 
+                           id="inputPassword" 
+                           className="form-control" 
+                           placeholder="Digite sua senha" 
+                           required />
+                    <label for="inputPassword">
+                        Sua senha
+                    </label>
+                </div>
+                <button className="btn btn-lg btn-primary btn-block" type="submit">
+                    <i className="fa fa-sign-in"></i> Acessar
+                </button>
+            </form>
+        );
     }
 }
 export default Login;
